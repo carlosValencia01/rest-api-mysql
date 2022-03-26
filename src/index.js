1856d1800dbd4f89118dad22180fb3e0b7013221
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-03-25 23:37:37 -0700

    feat:add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 // Settings
 app.set("port", process.env.PORT || 3000);
 // Middlewares
 app.use(express.json());
+app.use(cors());
 // Routes
 app.use(require("./routes/users"));
 
